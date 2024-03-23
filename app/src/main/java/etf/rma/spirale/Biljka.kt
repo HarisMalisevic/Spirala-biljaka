c3841e4f80e1fa12117bbfd449b617ede82e22ff
Haris Mališević <malisevic.haris@gmail.com>
2024-03-23 17:17:15 +0100

    Osnovni elementi gotovi

---
+++ app/src/main/java/etf/rma/spirale/Biljka.kt
@@ -0,0 +1,14 @@
+package etf.rma.spirale
+
+data class Biljka(
+    val naziv: String,
+    val porodica: String,
+    val medicinskoUpozorenje: String,
+    val medicinskeKoristi: List<MedicinskaKorist>,
+    val profilOkusa: ProfilOkusaBiljke,
+    val jela: List<String>,
+    val klimatskiTipovi: List<KlimatskiTip>,
+    val zemljisniTipovi: List<Zemljiste>
+){
+
+}
---
+++ app/src/main/java/etf/rma/spirale/Zemljiste.kt
@@ -0,0 +1,10 @@
+package etf.rma.spirale
+
+enum class Zemljiste(val naziv: String) {
+    PJESKOVITO("Pjeskovito zemljište"),
+    GLINENO("Glinеno zemljište"),
+    ILOVACA("Ilovača"),
+    CRNICA("Crnica"),
+    SLJUNOVITO("Šljunovito zemljište"),
+    KRECNJACKO("Krečnjačko zemljište");
+}
