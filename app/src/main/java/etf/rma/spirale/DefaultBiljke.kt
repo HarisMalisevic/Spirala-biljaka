26546236dc8022d4dd31821abea390330da90a36
Haris Mališević <malisevic.haris@gmail.com>
2024-04-02 15:31:37 +0200

    Testovi prošli

--- app/src/main/java/etf/rma/spirale/DefaultBiljke.kt
+++ app/src/main/java/etf/rma/spirale/DefaultBiljke.kt
@@ -10,102 +10,45 @@ val defaultBiljke = listOf(
         jela = listOf("Salata od paradajza", "Punjene tikvice"),
         klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUBTROPSKA),
         zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.ILOVACA)
-    ), Biljka(
+    ),
+    Biljka(
         naziv = "Nana (Mentha spicata)",
         porodica = "Lamiaceae (metvice)",
-        medicinskoUpozorenje = "Nije preporučljivo za trudnice, dojilje i djecu mlađu od 3 godine .",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.PROTIVBOLOVA
-        ),
+        medicinskoUpozorenje = "Nije preporučljivo za trudnice, dojilje i djecu mlađu od 3 godine.",
+        medicinskeKoristi = listOf(MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.PROTIVBOLOVA),
         profilOkusa = ProfilOkusaBiljke.MENTA,
         jela = listOf("Jogurt sa voćem", "Gulaš"),
         klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.UMJERENA),
         zemljisniTipovi = listOf(Zemljiste.GLINENO, Zemljiste.CRNICA)
-    ), Biljka(
+    ),
+    Biljka(
         naziv = "Kamilica (Matricaria chamomilla)",
         porodica = "Asteraceae (glavočike)",
         medicinskoUpozorenje = "Može uzrokovati alergijske reakcije kod osjetljivih osoba.",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.SMIRENJE, MedicinskaKorist.PROTUUPALNO
-        ),
+        medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.PROTUUPALNO),
         profilOkusa = ProfilOkusaBiljke.AROMATICNO,
         jela = listOf("Čaj od kamilice"),
         klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUBTROPSKA),
         zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
-    ), Biljka(
+    ),
+    Biljka(
         naziv = "Ružmarin (Rosmarinus officinalis)",
         porodica = "Lamiaceae (metvice)",
-        medicinskoUpozorenje = "Treba ga koristiti umjereno i konsultovati se sa ljekarom pri dugotrajnoj upotrebi ili upotrebi u većim količinama .",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.REGULACIJAPRITISKA
-        ),
+        medicinskoUpozorenje = "Treba ga koristiti umjereno i konsultovati se sa ljekarom pri dugotrajnoj upotrebi ili upotrebi u većim količinama.",
+        medicinskeKoristi = listOf(MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.REGULACIJAPRITISKA),
         profilOkusa = ProfilOkusaBiljke.AROMATICNO,
-        jela = listOf("Pečeno pile", "Grah", "Gulaš"),
+        jela = listOf("Pečeno pile", "Grah","Gulaš"),
         klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
         zemljisniTipovi = listOf(Zemljiste.SLJUNOVITO, Zemljiste.KRECNJACKO)
-    ), Biljka(
+    ),
+    Biljka(
         naziv = "Lavanda (Lavandula angustifolia)",
         porodica = "Lamiaceae (metvice)",
         medicinskoUpozorenje = "Nije preporučljivo za trudnice, dojilje i djecu mlađu od 3 godine. Također, treba izbjegavati kontakt lavanda ulja sa očima.",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.SMIRENJE, MedicinskaKorist.PODRSKAIMUNITETU
-        ),
+        medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.PODRSKAIMUNITETU),
         profilOkusa = ProfilOkusaBiljke.AROMATICNO,
         jela = listOf("Jogurt sa voćem"),
         klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
-        zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
-    ), Biljka(
-        naziv = "Bosiljak (Ocimum basilicum)",
-        porodica = "Lamiaceae (usnate)",
-        medicinskoUpozorenje = "Može iritati kožu osjetljivu na sunce. Preporučuje se oprezna upotreba pri korištenju ulja bosiljka.",
-        medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.REGULACIJAPROBAVE),
-        profilOkusa = ProfilOkusaBiljke.BEZUKUSNO,
-        jela = listOf("Salata od paradajza", "Punjene tikvice"),
-        klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUBTROPSKA),
-        zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.ILOVACA)
-    ), Biljka(
-        naziv = "Nana (Mentha spicata)",
-        porodica = "Lamiaceae (metvice)",
-        medicinskoUpozorenje = "Nije preporučljivo za trudnice, dojilje i djecu mlađu od 3 godine .",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.SMIRENJE
-        ),
-        profilOkusa = ProfilOkusaBiljke.MENTA,
-        jela = listOf("Jogurt sa voćem", "Gulaš"),
-        klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.UMJERENA),
-        zemljisniTipovi = listOf(Zemljiste.GLINENO, Zemljiste.CRNICA)
-    ), Biljka(
-        naziv = "Kamilica (Matricaria chamomilla)",
-        porodica = "Asteraceae (glavočike)",
-        medicinskoUpozorenje = "Može uzrokovati alergijske reakcije kod osjetljivih osoba.",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.SMIRENJE, MedicinskaKorist.PROTUUPALNO
-        ),
-        profilOkusa = ProfilOkusaBiljke.AROMATICNO,
-        jela = listOf("Čaj od kamilice"),
-        klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUBTROPSKA),
-        zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
-    ), Biljka(
-        naziv = "Ružmarin (Rosmarinus officinalis)",
-        porodica = "Lamiaceae (metvice)",
-        medicinskoUpozorenje = "Treba ga koristiti umjereno i konsultovati se sa ljekarom pri dugotrajnoj upotrebi ili upotrebi u većim količinama .",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.SMIRENJE, MedicinskaKorist.REGULACIJAPRITISKA
-        ),
-        profilOkusa = ProfilOkusaBiljke.AROMATICNO,
-        jela = listOf("Pečeno pile", "Grah", "Gulaš"),
-        klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
-        zemljisniTipovi = listOf(Zemljiste.SLJUNOVITO, Zemljiste.KRECNJACKO)
-    ), Biljka(
-        naziv = "LUDA Lavanda (Lavandula angustifolia)",
-        porodica = "Lamiaceae (metvice)",
-        medicinskoUpozorenje = "Nije preporučljivo za trudnice, dojilje i djecu mlađu od 3 godine. Također, treba izbjegavati kontakt lavanda ulja sa očima.",
-        medicinskeKoristi = listOf(
-            MedicinskaKorist.SMIRENJE, MedicinskaKorist.PODRSKAIMUNITETU, MedicinskaKorist.REGULACIJAPROBAVE
-        ),
-        profilOkusa = ProfilOkusaBiljke.AROMATICNO,
-        jela = listOf("Jogurt sa voćem"),
-        klimatskiTipovi = listOf(KlimatskiTip.SUBTROPSKA, KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
         zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
     )
 )
