6e16500008dad2a5ff4bfb700652d4fcbe1aa9b4
Haris Mališević <malisevic.haris@gmail.com>
2024-04-27 14:24:02 +0200

    Klasa za novu aktivnost i TODO Lista

---
+++ app/src/main/java/etf/rma/spirale/NovaBiljkaActivity.kt
@@ -0,0 +1,21 @@
+package etf.rma.spirale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NovaBiljkaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nova_biljka)
+
+        //TODO: EditText: nazivET, porodicaET, medicinskoUpozorenjeET, jeloET
+        //TODO: ListView: medicinskaKoristLV, klimatskiTipLV, zemljisniTipLV
+        //                profilOkusaLV, jelaLV
+        //TODO: Button: dodajJeloBtn, dodajBiljkuBtn, uslikajBiljkuBtn
+        //TODO: ImageView: slikaIV
+
+        //TODO: Validacija (setError), Slikanje, Povratak na MainAcitivty
+        //TODO: DODAVANJE U LISTU!
+        //TODO: TESTIRANJE
+    }
+}
