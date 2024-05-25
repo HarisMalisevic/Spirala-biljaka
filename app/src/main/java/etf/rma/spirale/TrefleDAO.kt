9eb1c37c14453e954dbbaffbe48a65ab90554d06
Haris Mališević <malisevic.haris@gmail.com>
2024-05-25 21:15:34 +0200

    Manifest permisije i TrefleDAO uspostavljen

---
+++ app/src/main/java/etf/rma/spirale/TrefleDAO.kt
@@ -0,0 +1,18 @@
+package etf.rma.spirale
+
+import android.graphics.Bitmap
+
+class TrefleDAO {
+
+    private fun getImage (biljka: Biljka) : Bitmap {
+        //TODO: Implementirati
+    }
+
+    private fun fixData(biljka: Biljka) : Biljka{
+        //TODO: Implementirati
+    }
+
+    private fun getPlantsWithFlowerColor (flower_color: String, substr: String) : List<Biljka>{
+        //TODO: Implementirati
+    }
+}
