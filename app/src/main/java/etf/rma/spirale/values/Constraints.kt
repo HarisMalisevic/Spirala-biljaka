22c9eb60952da019d8d87bf2da168375eb1dd4a7
Haris Mališević <malisevic.haris@gmail.com>
2024-05-27 22:06:02 +0200

    API_KEY i BASE_URL u Constraints.kt

---
+++ app/src/main/java/etf/rma/spirale/values/Constraints.kt
@@ -0,0 +1,9 @@
+package etf.rma.spirale.values
+
+class Constraints {
+
+    companion object {
+        const val BASE_URL = "https://trefle.io/"
+        const val API_TOKEN = "dQ-U3NITq16JJk5kThd_qavKBw03i9T8SH97R-ts3X8"
+    }
+}
