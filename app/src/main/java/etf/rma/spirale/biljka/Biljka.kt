7425fa0085c1ed203ee7f40e54c1502fbdfe4240
Haris Mališević <malisevic.haris@gmail.com>
2024-06-09 00:06:17 +0200

    BugFix - dodavanje medicinskog upozorenja

--- app/src/main/java/etf/rma/spirale/biljka/Biljka.kt
+++ app/src/main/java/etf/rma/spirale/biljka/Biljka.kt
@@ -30,36 +30,51 @@ data class Biljka(
         private var klimatskiTipovi: MutableList<KlimatskiTip> = mutableListOf()
         private var zemljisniTipovi: MutableList<Zemljiste> = mutableListOf()
 
-        fun setNaziv(naziv: String) = apply { this.naziv = naziv }
-        fun setPorodica(porodica: String) = apply { this.porodica = porodica }
-        fun setMedicinskoUpozorenje(medicinskoUpozorenje: String) =
-            apply { this.medicinskoUpozorenje = medicinskoUpozorenje }
+        fun setNaziv(naziv: String) {
+            this.naziv = naziv
+        }
 
-        fun addMedicinskoUpozorenje(medicinskoUpozorenje: String) =
-            apply { this.medicinskoUpozorenje.plus(medicinskoUpozorenje) }
+        fun setPorodica(porodica: String) {
+            this.porodica = porodica
+        }
 
-        fun addMedicinskaKorist(medicinskaKorist: MedicinskaKorist) =
-            apply { this.medicinskeKoristi.add(medicinskaKorist) }
+        fun setMedicinskoUpozorenje(medicinskoUpozorenje: String) {
+            this.medicinskoUpozorenje = medicinskoUpozorenje
+        }
 
-        fun setProfilOkusa(profilOkusa: ProfilOkusaBiljke) =
-            apply { this.profilOkusa = profilOkusa }
+        fun addMedicinskoUpozorenje(medicinskoUpozorenje: String) {
+            this.medicinskoUpozorenje = this.medicinskoUpozorenje.plus(medicinskoUpozorenje)
+        }
 
-        fun addJelo(jelo: String) = apply { this.jela.add(jelo) }
-        fun addKlimatskiTip(klimatskiTip: KlimatskiTip) =
-            apply { this.klimatskiTipovi.add(klimatskiTip) }
+        fun addMedicinskaKorist(medicinskaKorist: MedicinskaKorist) {
+            this.medicinskeKoristi.add(medicinskaKorist)
+        }
 
-        fun addZemljisniTip(zemljisniTip: Zemljiste) =
-            apply { this.zemljisniTipovi.add(zemljisniTip) }
+        fun setProfilOkusa(profilOkusa: ProfilOkusaBiljke) {
+            this.profilOkusa = profilOkusa
+        }
+
+        fun addJelo(jelo: String) {
+            this.jela.add(jelo)
+        }
+
+        fun addKlimatskiTip(klimatskiTip: KlimatskiTip) {
+            this.klimatskiTipovi.add(klimatskiTip)
+        }
+
+        fun addZemljisniTip(zemljisniTip: Zemljiste) {
+            this.zemljisniTipovi.add(zemljisniTip)
+        }
 
         fun build(): Biljka {
 
             if (naziv.isEmpty()) throw IllegalArgumentException("Naziv nije postavljen")
             if (porodica.isEmpty()) throw IllegalArgumentException("Porodica nije postavljena")
-            if (profilOkusa == null) throw IllegalArgumentException("Profil okusa nije postavljen")
+            // if (profilOkusa == null) throw IllegalArgumentException("Profil okusa nije postavljen")
             // if (klimatskiTipovi.isEmpty()) throw IllegalArgumentException("Mora biti bar jedan klimatski tip")
             // if (zemljisniTipovi.isEmpty()) throw IllegalArgumentException("Mora biti bar jedan tip zemljista")
-            if (medicinskeKoristi.isEmpty()) throw IllegalArgumentException("Mora biti bar jedna medicinska korist")
-            if (jela.isEmpty()) throw IllegalArgumentException("Mora biti bar jedno jelo")
+            // if (medicinskeKoristi.isEmpty()) throw IllegalArgumentException("Mora biti bar jedna medicinska korist")
+            // if (jela.isEmpty()) throw IllegalArgumentException("Mora biti bar jedno jelo")
 
             Log.d("BUILDER Porodica", porodica)
             return Biljka(
