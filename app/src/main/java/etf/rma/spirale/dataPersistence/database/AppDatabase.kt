be14a25acc6a77c358aa76377f0d868a66545e2c
Haris Mališević <malisevic.haris@gmail.com>
2024-06-22 12:52:20 +0200

    biljke-db kreirano

--- app/src/main/java/etf/rma/spirale/dataPersistence/database/AppDatabase.kt
+++ app/src/main/java/etf/rma/spirale/dataPersistence/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package etf.rma.spirale.dataPersistence.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import etf.rma.spirale.biljka.Biljka
@@ -11,4 +13,25 @@ import etf.rma.spirale.biljka.BiljkaBitmap
 abstract class AppDatabase : RoomDatabase() {
     abstract fun biljkaDao(): BiljkaDAO
     abstract fun biljkaBitmapDao(): BiljkaBitmapDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = buildRoomDB(context)
+                INSTANCE = instance
+                instance
+            }
+        }
+
+        private fun buildRoomDB(context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "biljke-db"
+            ).build()
+        }
+    }
 }
