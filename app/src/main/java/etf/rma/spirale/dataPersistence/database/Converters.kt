5f50715c85a9c619ae6da5ef2d09b6ae00dfbee8
Haris Mališević <malisevic.haris@gmail.com>
2024-06-22 12:45:54 +0200

    TypeConverter za biljku u bazu

---
+++ app/src/main/java/etf/rma/spirale/dataPersistence/database/Converters.kt
@@ -0,0 +1,61 @@
+package etf.rma.spirale.dataPersistence.database
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import etf.rma.spirale.biljka.KlimatskiTip
+import etf.rma.spirale.biljka.MedicinskaKorist
+import etf.rma.spirale.biljka.Zemljiste
+
+class Converters {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromMedicinskaKoristList(value: List<MedicinskaKorist>): String {
+        val type = object : TypeToken<List<MedicinskaKorist>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toMedicinskaKoristList(value: String): List<MedicinskaKorist> {
+        val type = object : TypeToken<List<MedicinskaKorist>>() {}.type
+        return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromStringList(value: List<String>): String {
+        val type = object : TypeToken<List<String>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toStringList(value: String): List<String> {
+        val type = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromKlimatskiTipList(value: List<KlimatskiTip>): String {
+        val type = object : TypeToken<List<KlimatskiTip>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toKlimatskiTipList(value: String): List<KlimatskiTip> {
+        val type = object : TypeToken<List<KlimatskiTip>>() {}.type
+        return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromZemljisteList(value: List<Zemljiste>): String {
+        val type = object : TypeToken<List<Zemljiste>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toZemljisteList(value: String): List<Zemljiste> {
+        val type = object : TypeToken<List<Zemljiste>>() {}.type
+        return gson.fromJson(value, type)
+    }
+}
