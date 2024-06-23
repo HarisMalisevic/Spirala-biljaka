4527fc75af5b257d424201c152830e164a857d4e
Haris Mališević <malisevic.haris@gmail.com>
2024-06-23 12:47:03 +0200

    Presao na HTTP

--- app/src/main/java/etf/rma/spirale/values/Constants.kt
+++ app/src/main/java/etf/rma/spirale/values/Constants.kt
@@ -9,7 +9,7 @@ import etf.rma.spirale.R
 
 object Constants {
 
-    const val BASE_URL = "https://trefle.io/"
+    const val BASE_URL = "http://trefle.io/"
     const val API_TOKEN = "dQ-U3NITq16JJk5kThd_qavKBw03i9T8SH97R-ts3X8"
 
     fun getDefaultBitmap(context: Context): Bitmap {
